810f483fbabb6fb0e2a662ca97051056530f2daa
Mo'tasem salem <msalem@smartchargerech.com>
2022-07-05 23:36:19 +0300

    remove heath

--- app/controllers/healths_controller.rb
+++
@@ -1,5 +0,0 @@
-class HealthsController < ApplicationController
-  def check
-    render status: :ok, json: { status: :ok, statusCode: 200 }.to_json
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  get "health", to: "healths#check"
   # path started -> api/v1
   namespace :api do
     namespace :v1 do
